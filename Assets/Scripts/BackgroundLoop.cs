442e3b131082c4930db1ff85f0cb47829cb3da82
tkddlaksek <80864758+tkddlaksek@users.noreply.github.com>
2021-04-08 10:12:31 +0900

    4월8일 기민재

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    private float width;
+    void Awake()
+    {
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x; //20.48
+    }
+
+    
+    void Update()
+    {
+        if(transform.position.x <= -width)
+        {
+            Repossition();
+        }
+    }
+    private void Repossition()
+    {
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
+}
